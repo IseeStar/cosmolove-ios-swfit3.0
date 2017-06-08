9d6858eacc7b06cc0008f4b0883f2ef67c9a036c
dongzhixiong10 <dongzhixiong10@163.com>
2017-06-08 18:42:23 +0800

    pod初始化

--- DZProjectswift/DZProjectswift/ViewController.swift
+++ DZProjectswift/DZProjectswift/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("点击屏幕")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
