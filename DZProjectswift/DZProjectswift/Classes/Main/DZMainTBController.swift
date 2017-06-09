b42b1a1f9869682c75c06ca3d6ed78f7d1872054
dongzhixiong10 <dongzhixiong10@163.com>
2017-06-09 18:32:45 +0800

    同步一下

---
+++ DZProjectswift/DZProjectswift/Classes/Main/DZMainTBController.swift
@@ -0,0 +1,41 @@
+//
+//  DZMainTBController.swift
+//  DZProjectswift
+//
+//  Created by 时尚致爱 on 2017/6/8.
+//  Copyright © 2017年 ShiShang. All rights reserved.
+//
+
+import UIKit
+
+class DZMainTBController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.yellow
+        
+        _ = ["hy","t"]
+        
+        _ = ("tt",89,"hh")
+        
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
